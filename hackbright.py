ade302f23bb543c3061ec154135df5ff8892a230
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-29 16:24:36 -0700

    Improved handle_input()

--- hackbright.py
+++ hackbright.py
@@ -116,9 +116,26 @@ def handle_input():
             github = args[0]
             get_student_by_github(github)
 
+    #input_string = "student marissaskud"
+    #tokens = ["student", 'marissaskud']
+    #command = ["student"]
+    #args = ["marissaskud"]
+
         elif command == "new_student":
             first_name, last_name, github = args  # unpack!
             make_new_student(first_name, last_name, github)
+
+        elif command == "project_title":
+            title = args[0]
+            get_project_by_title(title)
+
+        elif command == "grade_on_project":
+            username, project = args
+            get_grade_by_github_title(username, project)
+
+        elif command == "give_grade":
+            username, project_title, grade = args
+            assign_grade(username, project_title, grade)
 
         else:
             if command != "quit":
@@ -128,7 +145,7 @@ def handle_input():
 if __name__ == "__main__":
     connect_to_db(app)
 
-    #handle_input()
+    handle_input()
 
     # To be tidy, we close our database connection -- though,
     # since this is where our program ends, we'd quit anyway.
